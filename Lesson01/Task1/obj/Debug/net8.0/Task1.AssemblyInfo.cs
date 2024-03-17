d30bc986c4e5465695a15e800fc899a488992358
Elijah <il.klim777@mail.ru>
2024-03-17 22:42:25 +0300

    add smth

--- Lesson01/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson01/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb72728391a22374bdfec719bbaace478358ca95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455212043cf970ada8857a04fd3f1ebd69f95139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

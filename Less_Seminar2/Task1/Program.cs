ca226050f9fe79ac182f7ac9961dc5a3424e6f42
Elijah <il.klim777@mail.ru>
2024-03-20 15:17:36 +0300

    execute T1

---
+++ Less_Seminar2/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу: на вход 3-х знач-ое число и удаляет 2-ю цифру этого числа.
+// a = 256 => 26
+// a = 891 => 81
+
+
+Console.WriteLine("Введите 3-х значное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >=100 && num <= 999 || num <= -100 && num >= -999)
+{
+    int digit1 = num / 100;
+    int digit3 = num % 10;
+    int result = digit1*10 + digit3;
+    Console.Write(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}

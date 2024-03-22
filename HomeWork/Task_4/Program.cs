beb97f8551efb2d8346df8d7f71fc49f22f52191
Elijah <il.klim777@mail.ru>
2024-03-22 16:38:43 +0300

    T_4

---
+++ HomeWork/Task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Прорамма: на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите натуральное число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10)
+{
+    Console.WriteLine(num);
+}
+else
+    {
+      while (num > 0)
+      {
+        int currentdigit = num / 10;
+        num %= 10;
+        if (num > 0)
+            {
+            Console.Write(currentdigit + " , ");
+            }
+        else
+        {
+            Console.Write(currentdigit);
+        }
+
+      }
+    }
+   

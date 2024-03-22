00bd42402d2a935ac6777c7aa5a60e28413a2dd5
Elijah <il.klim777@mail.ru>
2024-03-22 12:41:46 +0300

    T1

--- HomeWork/Task_1/Program.cs
+++ HomeWork/Task_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу: принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+int num;
+
+Console.WriteLine("Ввод числа");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 || num % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+    
+}
+
+
+

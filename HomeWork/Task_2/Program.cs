1f8d8ab277da0d3d1e043bdd26946f1216a689dc
Elijah <il.klim777@mail.ru>
2024-03-22 13:42:04 +0300

    T_2

---
+++ HomeWork/Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа: принимает на вход координаты Х и Y, 
+// причем Х и Y не равны 0 и выдает номер координатной четверти плоскости, в которой эта точка.
+
+int x;
+int y;
+
+Console.WriteLine("Ввести х");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввести y");
+y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("1-я четверть");
+else if (x < 0 && y > 0) Console.WriteLine("2-я четверть");
+else if (x < 0 && y < 0) Console.WriteLine("3-я четверть");
+else if (x > 0 && y < 0) Console.WriteLine("4-я четверть");
+else  Console.WriteLine("Ошибка!");

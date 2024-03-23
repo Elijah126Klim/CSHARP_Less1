4da4d51926ffda87f2fc8dcb8027e61d083d4058
Elijah <il.klim777@mail.ru>
2024-03-24 00:46:57 +0300

    Execute T_3

---
+++ Lesson02/Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача № 3
+// Дано N гирь с разным весом, найти вес самой тяжелой гири. // Дано 100 гирь, найти вес самой тяжелой.
+
+int n = 5;
+int[] array = { 9, 4, 7, 3, 2 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+    max = array[i]; // текущий эл-т массива
+    }
+    i = i +1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)   // j = j + 1
+{ 
+    if (array[j] > max)
+    {
+    max = array[j]; // текущий эл-т массива
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array) // нет индекса
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);

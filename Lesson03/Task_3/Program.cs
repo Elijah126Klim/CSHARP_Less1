7e4d8e4877341f37e0053f26a0fe6407337db589
Elijah <il.klim777@mail.ru>
2024-03-25 03:21:09 +0300

    Execute tasks

---
+++ Lesson03/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Обнуление четных эл-тов массива (Ф измен-ия массива)
+
+void ZeroEvenElements(int[] arr) // Ф принимает целочисленный массив
+{
+    for (int i = 0; i < arr.Length; i++) // Перебрать все эл-ты массива для поиска четнных
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+
+    }
+}
+
+void PrintArray(int[] arr) // Для удобства вывода массива на экран
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 }; // Создали массив
+ZeroEvenElements(array);
+PrintArray(array);

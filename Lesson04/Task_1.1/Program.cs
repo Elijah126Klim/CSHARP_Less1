121f466b04b24dc1374a0e09cbda0dada6ade818
Elijah <il.klim777@mail.ru>
2024-03-28 23:53:30 +0300

    Less04:Task1

---
+++ Lesson04/Task_1.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача № 1.1 Напишем функцию создания двум-ого массива
+
+int[,] CreateMatrix(int rowcount, int columCount)
+{
+    int[,] matrix = new int[rowcount, columCount];
+
+    Random rnd = new Random();
+    // Заполняем массив на экран
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);

d7adc002c90212c7221a7389d0059067690ef451
Elijah <il.klim777@mail.ru>
2024-03-30 03:19:56 +0300

    Execute tasks

---
+++ Lesson04/Task_2/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача № 2
+// Назовем число "интересным", если его сумма цифр четная
+// Создать двумерный массив, состоящий из целых чисел. Вывести на экран "интерес-ые" эл-ты массива
+
+int[,] CreateMatrix(int rowcount, int columCount)
+{
+    int[,] matrix = new int[rowcount, columCount];
+
+    Random rnd = new Random();
+    // Заполняем массив на экран
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4); // Точка входа
+ShowMatrix(matrix);
+foreach (int e in matrix)
+{
+    if (IsInteresting(e) == true)
+    {
+        Console.WriteLine(e);
+    }
+
+}
+bool IsInteresting(int value) // Реализация для функции IsInteresting
+{
+    int sumOfDigits = GetsSumOfDigits(value);
+    if (sumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    //else
+    //{
+    return false;
+    //}
+}
+// Реализация фунуции GetsSumOfDigits
+int GetsSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10; // получим правую цифру нашего числа
+        value = value / 10; // перейти к след цифре
+    }
+    return sum;
+}
+
---
+++ Lesson04/Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача № 3 Считать с консоли строку, состоящую из цифр и лат. букв
+// Сформировать новую строку из букв исходной строки.
+
+string Getletters(string s) // Функция возвр-я строки, принимает на вход строку
+{     // код функции
+    string letters = ""; // строковая переменная, будет хранить буквы, извлеченные из строки
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+
+        }
+
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = Getletters(str);
+Console.WriteLine(result);

290b0ee2ee206ad9ea7c08c2ce0868b9668c9bff
Elijah <il.klim777@mail.ru>
2024-04-04 22:15:34 +0300

    execute Hmw_3

---
+++ Homework_3/Task_3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является      ли      она      палиндромом.
+
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да “55655” => Да
+
+Console.WriteLine("Введите строку");
+string userInput = Console.ReadLine();
+
+bool palindrome = true;
+
+for (int i = 0; i < userInput.Length / 2; i++) // так как сравниваем сразу пару
+{
+    if (userInput[i] != userInput[userInput.Length - i - 1]) // != обратно
+    {
+        palindrome = false; // присвоение
+        break;
+    }
+
+}
+
+Console.WriteLine(palindrome ? "Строка палиндром" : "Строка не падиндром");

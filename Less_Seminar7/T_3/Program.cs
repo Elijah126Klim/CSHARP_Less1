673d774088255d3c46c0f4d55ab364e8676c7f51
Elijah <il.klim777@mail.ru>
2024-04-06 21:40:13 +0300

    Sem_7

---
+++ Less_Seminar7/T_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l   “World” => W r l d    “Hello world!” => Hllwrld
+
+void PrintConsonants(string str, int count = 0)
+{
+    if (count == str.Length) return;
+    string vowels = "aouiey";
+    if (char.IsAsciiLetter(str[count]) == true && !vowels.Contains(char.ToLower(str[count]))) // если символ явл-ся буквой и не явля-ся гласной
+    {
+        Console.Write(str[count] + " ");
+    }
+
+        PrintConsonants(str, count + 1);
+}
+
+void PrintConsonants2(string str)
+{
+    if (str.Length == 0) return;
+    string vowels = "aouiey";
+    if (char.IsAsciiLetter(str[0]) == true && !vowels.Contains(char.ToLower(str[0]))) // если символ явл-ся буквой и не явля-ся гласной
+    {
+        Console.Write(str[0] + " ");
+    }
+
+        PrintConsonants2(str.Substring(1)); // рекурсивный вызов / дойдем до пустой строки
+}
+
+Console.WriteLine("Введите строку латинскими буквами");
+string userInput = Console.ReadLine();
+
+PrintConsonants(userInput);
+PrintConsonants2(userInput);
+
---
+++ Less_Seminar7/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+//натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+
+Console.WriteLine("Ввод натур-ого числа: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNtauralNumbers(number); // вызов метода
+
+void PrintNtauralNumbers(int num) // дойти но 0; N=5 / без возврата знач-ия
+{
+    if(num == 0) return; // как только 0 => выход
+    PrintNtauralNumbers(num - 1); // сохраняются в стек
+    Console.Write($"{num} "); // из стека
+}
+
+
+
+
